a52ae0801bb6677d0c6cce36a1e1de7defdf23c9
juankcol <jamayafl15dw@ikzubirimanteo.com>
2016-10-05 07:25:13 +0000

    no funciona nada

--- paginas/addTareas.php
+++ paginas/addTareas.php
@@ -6,9 +6,10 @@
     $descripcion;
         
     try{
-        $sth=$mbd->prepare("insert into 'tareas' values(".$id.",".$nombre.",".$descripcion.");");
+        $sth=$mbd->prepare("insert into tareas values(".$id.", '".$nombre."','".$descripcion."');");
         $sth->execute();
         $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+        
     }catch(PDOException $e){
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
--- paginas/delTareas.php
+++ paginas/delTareas.php
@@ -6,9 +6,10 @@
     $descripcion;
         
     try{
-        $sth=$mbd->prepare("delete from 'tareas' where id =".$id);
+        $sth=$mbd->prepare("delete from tareas where ID =".$id.";");
         $sth->execute();
         $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+        
     }catch(PDOException $e){
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
